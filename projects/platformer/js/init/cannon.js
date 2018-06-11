d92b61ccc599571fcea5f2e2f0f6b09119224300
Sam Jones <jones.sam329@gmail.com>
2018-06-11 16:22:07 +0000

    create awesome platformer game

--- projects/platformer/js/init/cannon.js
+++ projects/platformer/js/init/cannon.js
@@ -23,7 +23,7 @@
         cannon.create.onTop(450);
         cannon.create.onBottom(300);
         cannon.create.onLeft(200);
-        cannon.create.onRight(100);
+        cannon.create.onRight(200, 1);
         
         
         
